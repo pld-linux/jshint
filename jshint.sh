7f6eb34bcd557047ca5f728918a5ef3906f961bd
Elan Ruusamäe <glen@pld-linux.org>
2011-05-24 06:17:24 +0000

    - use jpackage utils
    Changed files:
        jshint.sh -> 1.2

--- jshint.sh
+++ jshint.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-java -jar /usr/share/java/js.jar /usr/share/jshint/env/rhino.js "$@"
+set -e
+
+if [ ! -f /usr/share/java-utils/java-functions ]; then
+	echo >&2 "jpackage-utils not found."
+	exit 1
+fi
+. /usr/share/java-utils/java-functions
+
+set_javacmd
+rhino=$(find-jar js)
+
+exec $JAVACMD -jar $rhino /usr/share/jshint/env/rhino.js "$@"
